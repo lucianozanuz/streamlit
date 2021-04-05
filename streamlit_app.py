80080da808df88a1be71f3282bb290b0eafd1f94
lucianozanuz <lzanuz@hotmail.com>
2021-04-05 07:30:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,3 +68,14 @@ add_slider = st.sidebar.slider(
     'Select a range of values',
     0.0, 100.0, (25.0, 75.0)
 )
+
+left_column, right_column = st.beta_columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
