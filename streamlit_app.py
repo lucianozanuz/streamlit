fe294309542d590dfd140d546b4834139dd82d99
lucianozanuz <lzanuz@hotmail.com>
2021-04-05 06:38:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,30 +7,13 @@ st.title('My first app')
 # Add some text
 st.text('Streamlit is great')
 
-st.dataframe(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
-
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
-
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
-
 if st.checkbox('Show dataframe'):
     st.dataframe(pd.DataFrame({
+        'first column': [1, 2, 3, 4],
+        'second column': [10, 20, 30, 40]
+    }))
+
+    st.write(pd.DataFrame({
         'first column': [1, 2, 3, 4],
         'second column': [10, 20, 30, 40]
     }))
